128d6e3dc98430a70b723f7a2cf97a4a66130802
Sean Cloudsdale <sean@Seans-MacBook-Pro.local>
2020-09-04 14:41:09 +0100

    new footer

--- footer.php
+++ footer.php
@@ -4,46 +4,24 @@
 					<div class="line span12"></div>
 				<div class="footerSection">
 					<div class="footerTop">
-						<h3>TRADE</h3>
-
-					</div>
-					<div>
-							<p>23 Frith Street</p>
-						<p>Soho</p>
-						<p>London</p>
-						<p>W1D 4RR</p>
+						<a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/google.svg" alt="Get it on Google"></a>
 					</div>
 					<div class="footerBottom">
 						
-						<a href="https://goo.gl/maps/9JRrQHumpq82" target="_blank"><p><i class="fas fa-map-marked-alt"></i> View on Google Maps</p></a>
+						
 					</div>
 				</div>
 				<div class="footerSection">
-					<div class="footerTop">
-						<h3>OPENING HOURS</h3>
+				<div class="footerTop">
+						<a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/apple.svg" alt="Get it on Apple"></a>
 					</div>
 					<div class="footerBottom">
-						<p>Monday to Friday:</p> 
-						<p>11am-3am </p>
-						<p>Saturday & Bank Holidays:</p>
-						<p>5pm-3am</p>
-						<p>(kitchen closes at 2am) </p>
-						<p>Sunday: </p>
-						<p>Closed</p>
- 
+						
+						
 					</div>
 				</div>
 				<div class="footerSection">
-					<div class="footerTop">
-						<h3>RESERVATIONS</h3>
-						<a href="tel:02074341724"><p><i class="fas fa-phone"></i> 020 7434 1724</p></a>
-						<a href="mailto:reservations@tradesoho.co.uk"><p><i class="far fa-envelope"></i> reservations@tradesoho.co.uk</p></a>
-						<a href="https://www.sevenrooms.com/reservations/tradesoho"><button class="btn">BOOK</button></a>
-					</div>
-					<div class="footerBottom">
-						<h3>MEMBERSHIP ENQUIRIES</h3>
-						<a href="mailto:member@tradesoho.co.uk"><p><i class="far fa-envelope"></i> member@tradesoho.co.uk</p></a>
-					</div>
+					
 				</div>
 				<div class="footerSection" id="footerSocial">
 					<div class="footerTop">
@@ -51,14 +29,21 @@
 						&nbsp&nbsp&nbsp<a href="https://www.instagram.com/tradesoho"><i class="fab fa-instagram fa-lg"></i></a>
 					</div>
 					<div class="footerBottom">
-						<a href="<?php echo get_home_url(); ?>/careers"><p>Careers</p></a>
+						<a href="mailto:hello@tradehospitality.co.uk"><p>Contact: hello@tradehospitality.co.uk</p></a>
+						<a href="<?php echo get_home_url(); ?>/privacy"><p>Privacy Policy</p></a>
+						<a href="<?php echo get_home_url(); ?>/about"><p>About us</p></a>
 					
-						<a href="https://seancloudsdale.com/"><p>Website & Photo Credit</p></a>
+
+					
+						
 					</div>
-
+				
 				</div>
+				
 			</footer>
-
+			<div class="span12" style="text-align: center; padding-bottom: 1em;">
+					<a href="https://seancloudsdale.com/"><p>A Cloudsdale Website</p></a>
+					</div>
 		</div>
 	</div>
 			<!-- /footer -->
